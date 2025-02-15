5773c183ee85ed271833165e096bc7f4f6eb62b8
ShaunMWallace <9406875+ShaunMWallace@users.noreply.github.com>
2025-02-15 13:40:02 -0500

    use correct path for problem matcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp /action/problem-matcher.json /github/workflow/problem-matcher.json
+cp ./problem-matcher.json /github/workflow/problem-matcher.json
 
 git clone --depth 1 -b 3.1.0 https://github.com/WordPress/WordPress-Coding-Standards.git ~/wpcs
 
